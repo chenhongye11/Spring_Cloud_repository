16d1e505a74feb028786a9ad4904bcef414ed066
Hongye Chen <56781336+Peterchyy@users.noreply.github.com>
2020-09-09 00:14:16 -0400

    this is the parent of all the spring cloud

---
+++ sp02-itemService/src/test/java/com/hongye/sp02/Sp01ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongye.sp02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp01ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
